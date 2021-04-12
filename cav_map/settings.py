373582fa5164f0a6ded5d85582aa1a234d78bd1f
Atharv <61912695+akhatod117@users.noreply.github.com>
2021-04-12 14:47:34 -0400

    changing to personal-testing
    deploying new heroku app to test

--- cav_map/settings.py
+++ cav_map/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '@f2&r1*2m^u!k^+&h%w&vmj9k&%4#@9_z+%s!7s2nd+5ar0v-!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['a01-cav-map.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['a01-cav-map.herokuapp.com','cavmap-personal-testing.herokuapp.com','127.0.0.1']
 
 
 # Application definition
